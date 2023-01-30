1848f6bcb78712f90fe7468d2bc5d13206a38c57
Michael Totty <mictott@gmail.com>
2023-01-30 16:47:53 -0500

    edited s01_convert_SRA_to_FASTq.sh

--- code/s01_convert_SRA_to_FASTQ.sh
+++ code/s01_convert_SRA_to_FASTQ.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
-#$ -pe local 6
-#$ -l mem_free=248G
+#$ -l mem_free=64G
+#$ -l h_vmem=64G
 #$ -l h_fsize=200G
-#$ -o /dcs04/lieber/marmaypag/Allaway_mouseDlx_LIBD4125/FASTQ
+#$ -o logs/mouseDLX_convert_SRA.txt
+#$ -e logs/mouseDLX_convert_SRA.txt
 #$ -m e
 #$ -M michael.totty@libd.org
+
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SGE_TASK_ID}"
 
 # load SRAtoolkit
 module load sratoolkit
@@ -17,3 +28,6 @@ module load sratoolkit
 # --spilt-files to seperate forward and reverse reads into seperate files
 # -O output directory
 fastq-dump --gzip --split-files SRR13402808 -O /dcs04/lieber/marmaypag/Allaway_mouseDlx_LIBD4125/raw-data/FASTQ
+
+echo "**** Job ends ****"
+date
